f96fd95d564fbf4a831feb325271186ff5dd1305
AthamaRD <89761034+AthamaRD@users.noreply.github.com>
2021-10-11 15:37:42 +0700

    Add files via upload

---
+++ Chapter 4 Latihan/No.5 Chapter 4.py
@@ -0,0 +1,4 @@
+jumlahLakilaki = int(input("Masukkan jumlah mahasiswa laki - laki: "))
+jumlahPerempuan = int(input("Masukkan jumlah mahasiswa perempuan: "))
+print("\nLaki - Laki  :" ,"@" * (jumlahLakilaki // 7), jumlahLakilaki)
+print("Perempuan    :", "@" * (jumlahPerempuan // 7), jumlahPerempuan)
